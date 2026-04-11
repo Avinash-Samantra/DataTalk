70f21dc6719e9816805957d5d5194632cfc3f986
Avinash-Samantra <avinash021752gmail.com>
2026-04-12 01:15:35 +0530

    remove test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import pymupdf
-
-doc = pymupdf.open("C:/Users/avina/Downloads/Subject_Syllabus_030705506.pdf")
-page = doc.load_page(0)
-pix = page.get_pixmap()
-# pix.save(f"page-{page.number}.png")
-help(pymupdf.open)
