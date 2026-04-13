76b24955409402ca79f3ae1d4f6041f764e00f4a
Avinash-Samantra <avinash021752gmail.com>
2026-04-14 03:00:51 +0530

    fix syntax error

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from groq import Groq
 from dotenv import load_dotenv
 
 load_dotenv()
-client = Groq(api_key=st.secrets("GROQ_API_KEY"))
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 st.title("DataTalk")
 st.write("Every Information you need from csv and pdf's at you fingertip")
